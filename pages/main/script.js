b97661b45445818077f4147b6a4699bbf17fd9a3
Danil Chemodanov <dan.cmdn@gmail.com>
2020-10-27 02:10:47 +0300

    feat: add burger-menu

---
+++ pages/main/script.js
@@ -0,0 +1,14 @@
+const menuIcon = document.querySelector('.burger-menu__button');
+const navbar = document.querySelector('.burger-menu');
+const overlay = document.querySelector('.overlay');
+
+menuIcon.addEventListener('click', () => {
+	navbar.classList.toggle('burger-menu_active');
+	menuIcon.classList.toggle('rotate');
+	overlay.classList.toggle('overlay_active');
+});
+overlay.addEventListener('click', () => {
+	navbar.classList.toggle('burger-menu_active');
+	menuIcon.classList.toggle('rotate');
+	overlay.classList.toggle('overlay_active');
+})
